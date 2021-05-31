90686b0d4cbcdddfd4ed7de02db2e5df215f8e18
Nick Ng <nickngqs@gmail.com>
2021-05-31 15:15:29 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1 :style="h1styles">HMR doesn't take effect - {{ msg }}</h1>
+  <h1 :style="{ fontSize: `${fontSize}px` }">HMR take effect - {{ msg }}</h1>
+</template>
+
+<script setup>
+import { defineProps, ref } from 'vue'
+
+const props = defineProps({
+  fontSize: String,
+  msg: String
+})
+
+const h1styles = ref({
+  fontSize: `${props.fontSize}px`
+})
+</script>
